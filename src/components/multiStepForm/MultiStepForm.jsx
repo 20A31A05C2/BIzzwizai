6f6b86eb8f0a780500a2255f76aaf4ee8d382c66
Pothuraju Vineel <102516151+20A31A05C2@users.noreply.github.com>
2025-09-05 22:19:02 +0530

    Update MultiStepForm.jsx

--- src/components/multiStepForm/MultiStepForm.jsx
+++ src/components/multiStepForm/MultiStepForm.jsx
@@ -18,7 +18,7 @@ import { toast } from '@/components/ui/use-toast';
 import ApiService from '@/apiService';
 import { useTranslation } from 'react-i18next';
 import LevelHeader from '@/components/multiStepForm/LevelHeader';
-import TopHeaderBar from '@/components/multiStepForm/TopHeaderbar';
+import TopHeaderBar from "./TopHeaderBar";
 
 const MultiStepForm = ({ onSubmit: onSubmitExternal, mode = 'register' }) => {
   const {
